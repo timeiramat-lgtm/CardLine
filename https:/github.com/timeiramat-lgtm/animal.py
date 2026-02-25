14fa5195ee2e91df68f4849e4b3bd7550755f6ff
timeiramat-lgtm <timei.ramat@gmail.com>
2026-02-25 15:50:37 +0100

    Delete https:/github.com/timeiramat-lgtm directory

--- https:/github.com/timeiramat-lgtm/animal.py
+++
@@ -1,9 +0,0 @@
-class Animal:
-    def __init__(self , nom: str, nomscientif: str, taille: int):
-        self.nom = nom
-        self.nomscientif = nomscientif
-        self.taille = taille
-
-        def __str__(self) -> str:
-            return '...' + self.getnom() + '...'
-
